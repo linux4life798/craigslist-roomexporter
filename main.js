b4902c0fb3b250a41f047408b55889510f631fd9
Craig Hesling <craig@hesling.com>
2019-01-31 13:00:09 -0800

    Delay requests as to not get blocked and save raw listing/details results

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
+const fs = require('fs');
 const craigslist = require('node-craigslist');
 const createCsvWriter = require('csv-writer').createArrayCsvWriter;
+
+const csvFileName = "sfbay.csv";
+const rawResultsFile = "results.json";
+const maxDelaySeconds = 3; // for fetching details
+const maxTitlePreviewLength = 40;
+
 const csvWriter = createCsvWriter({
-    header: ['TITLE', 'LOCATION', 'PRICE', 'DESCRIPTION', 'URL', 'MAPURL', 'MAPKEYWORD'],
-    path: 'sfbay.csv'
+    header: ['TITLE', 'LOCATION', 'PRICE', 'DESCRIPTION', 'URL', 'MAPURL', 'MAPKEYWORD', 'PID'],
+    path: csvFileName
 });
-
-const records = [
-    ['Bob',  'French, English'],
-    ['Mary', 'English']
-];
-
-// csvWriter.writeRecords(records)       // returns a promise
-    // .then(() => {
-    //     console.log('...Done');
-    // });
 
 var client = new craigslist.Client({
     city : 'sfbay'
@@ -26,11 +23,18 @@ var client = new craigslist.Client({
     // minAsk : '100'
   };
 
+var allResults = [];
+
 var processListing = function(list, details) {
-    console.log("Listing: ", list);
-    console.log('\n\n\n');
-    console.log("Details: ", details);
-    console.log('\n\n\n');
+    // console.log("Listing: ", list);
+    // console.log('\n\n\n');
+    // console.log("Details: ", details);
+    // console.log('\n\n\n');
+
+    allResults.push({list: list, details: details});
+
+    // Print the preview
+    console.log("Listing:", list.title.substring(0,maxTitlePreviewLength), "|", list.url)
 
     var mapkeyword = "";
     if ('mapUrl' in details) {
@@ -55,31 +59,65 @@ var processListing = function(list, details) {
     }
 
     // details.description = "";
-    var info = [details.title, list.location, list.price, details.description, details.url, details.mapUrl, mapkeyword];
+    var info = [details.title, list.location, list.price, details.description, details.url, details.mapUrl, mapkeyword, list.pid];
     // console.log(info.join(' | '));
     csvWriter.writeRecords([ info ])
         .then(() => {
             // console.log('...Done');
         });
 }
+function writeListingsToFile(results) {
+    console.log("# Writing to", rawResultsFile);
+    let data = JSON.stringify(results, null, 2);
+    fs.writeFileSync(rawResultsFile, data, (err) => {
+        if (err) throw err;
+        console.log('Data written to file');
+    });
+    fs.appendFile(rawResultsFile, '\n', function (err) {
+        if (err) throw err;
+        console.log('Newline written to file');
+      });
+}
 
-// client
-//     .list(options)
-//     .then((listings) => { console.log(listings[4]); console.log('\n\n\n'); return client.details(listings[4]); })
-//     .then((details) => {
-//         processListing('', details);
-//     })
-//     .catch((err) => {
-//         console.error(err);
-//     });
+var listsIndex = 0;
+var scheduleFetchDetails = function(lists) {
+
+    delay = 1000 * ((Math.random()*10) % maxDelaySeconds);
+    console.log('# Waiting ', delay/1000, 'seconds')
+
+    setTimeout(function(lists) {
+        if (lists.length === undefined || lists.length == 0) {
+            return;
+        }
+
+        console.log("# Fetching details for index", listsIndex);
+        var list = lists[listsIndex++];
+        client.details(list, function (err, details) {
+            if (err != undefined) {
+                console.log("Error while fetching details:", err)
+            } else {
+                processListing(list, details);
+                if (listsIndex < lists.length) {
+                    console.log("# Schedule next details fetch for index", listsIndex);
+                    scheduleFetchDetails(lists);
+                } else {
+                    console.log("# Finished fetching details");
+                    writeListingsToFile(allResults);
+                }
+            }
+        })
+    }, delay, lists);
+}
 
 client
     .list(options)
     .then((listings) => {
-        var list = listings[4];
-        client.details(list, function (err, details) {
-            processListing(list, details);
-        });
+        console.log("# Found", listings.length, "Craigslist posts")
+        console.log("# This operation should take approx", (listings.length*(maxDelaySeconds/2)) / 60, "mins");
+        // var lists = listings.slice(0,1);
+        var lists = listings; // all listings
+        console.log("# Getting more details for", lists.length, "Craigslist posts")
+        scheduleFetchDetails(lists);
     })
     // .then((details) => {
     //     processListing('', details);
@@ -87,8 +125,6 @@ client
     .catch((err) => {
         console.error(err);
     });
-
-
 
 // client
 //   .search(options, '')
